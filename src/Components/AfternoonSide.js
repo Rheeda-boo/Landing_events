e183aac3568de7c4b841c13513c7ebb1048db11c
Rheeda-boo <rheeda.beecha@gmail.com>
2020-09-24 17:23:37 +0100

    Morning and afternoon event image captions updated

--- src/Components/AfternoonSide.js
+++ src/Components/AfternoonSide.js
@@ -7,19 +7,30 @@ export default function AfternoonSide() {
             <h1>Afternoon Events</h1>
             <figure className = "one">
                 <img src ="https://images.pexels.com/photos/1089438/pexels-photo-1089438.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt = "Event" />
-                <figcaption> Here!!</figcaption>
+                <figcaption> 
+                            <a href = "/"><b>Thu, Sep 24, 2020 6:30 PM </b><br/><i>A LADY'S GUIDE TO BOTANY</i></a>
+                    </figcaption>
             </figure>
+
             <figure className = "two">
                 <img src ="https://images.pexels.com/photos/3184418/pexels-photo-3184418.jpeg?cs=srgb&dl=pexels-fauxels-3184418.jpg&fm=jpg" alt = "Event" />
-                <figcaption> Here!!</figcaption>
+               <figcaption> 
+                            <a href = "/"><b>Thu, Sep 24, 2020 6:30 PM </b><br/><i>A LADY'S GUIDE TO BOTANY</i></a>
+                    </figcaption>
             </figure>
+
             <figure className = "three">
                 <img src ="https://images.pexels.com/photos/2740956/pexels-photo-2740956.jpeg?cs=srgb&dl=pexels-prateek-katyal-2740956.jpg&fm=jpg" alt = "Event" />
-                <figcaption> Here!!</figcaption>
+                <figcaption> 
+                            <a href = "/"><b>Thu, Sep 24, 2020 6:30 PM </b><br/><i>A LADY'S GUIDE TO BOTANY</i></a>
+                    </figcaption>
             </figure>
+
             <figure className = "four">
                 <img src ="https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt = "Event" />
-                <figcaption> Here!!</figcaption>
+                <figcaption> 
+                        <a href = "/"><b>Thu, Sep 24, 2020 6:30 PM </b><br/><i>A LADY'S GUIDE TO BOTANY</i></a>
+                    </figcaption>
             </figure>
         
         </div>
